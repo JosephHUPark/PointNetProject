a7a3906f990e7c885d49112acfbf2b1cd684bd74
Joseph Park <46663757+JosephHUPark@users.noreply.github.com>
2026-04-29 23:09:47 -0400

    Fixed Residual networks, added label smoothing to support regularization

--- models/pointnet_sem_seg.py
+++ models/pointnet_sem_seg.py
@@ -20,19 +20,28 @@ class get_model(nn.Module):
         self.bn3 = nn.BatchNorm1d(128)
         self.dp1 = nn.Dropout(p=0.3)
         self.dp2 = nn.Dropout(p=0.3)
+        
+        self.res_conv = nn.Conv1d(512, 256, 1)
 
     def forward(self, x):
         batchsize = x.size()[0]
         n_pts = x.size()[2]
+    
         x, trans, trans_feat = self.feat(x)
-        x = F.relu(self.bn1(self.conv1(x)))
-        res = x
-        x = self.dp1(F.relu(self.bn2(self.conv2(x))))
+        x = F.relu(self.bn1(self.conv1(x)))   # [B,512,N]
+    
+        res = self.res_conv(x)                # [B,256,N]
+        x = F.relu(self.bn2(self.conv2(x)))   # [B,256,N]
+        x = self.dp1(x)
         x = x + res
-        x = self.dp2(F.relu(self.bn3(self.conv3(x))))
+    
+        x = F.relu(self.bn3(self.conv3(x)))   # [B,128,N]
+        x = self.dp2(x)
         x = self.conv4(x)
+    
         x = x.transpose(2,1).contiguous()
         x = x.view(batchsize, n_pts, self.k)
+    
         return x, trans_feat
 
 class get_loss(torch.nn.Module):
@@ -41,7 +50,7 @@ class get_loss(torch.nn.Module):
         self.mat_diff_loss_scale = mat_diff_loss_scale
 
     def forward(self, pred, target, trans_feat, weight):
-        loss = F.cross_entropy(pred, target, weight = weight)
+        loss = F.cross_entropy(pred, target, weight=weight, label_smoothing=0.1)
         mat_diff_loss = feature_transform_reguliarzer(trans_feat)
         total_loss = loss + mat_diff_loss * self.mat_diff_loss_scale
         return total_loss
